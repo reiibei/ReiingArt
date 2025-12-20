9c7bec4a695619b0827a981c1692ea346d5c25f4
cosyreii <reishi2023@gmail.com>
2025-12-20 21:20:15 +0800

    aquarium date

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -2,6 +2,6 @@
 	<title>Portfolio</title>
 </svelte:head>
 
-<p class="mx-auto w-11/12">
-hi
+<p class="mx-auto w-11/12 min-h-100">
+	<img src="https://i.imgur.com/yxVbTJ2.jpeg" alt="aquarium date" />
 </p>
