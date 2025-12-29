349b32a822efb8d88eb1e25e194cffb0ba9a1f1d
cosyreii <reishi2023@gmail.com>
2025-12-29 14:04:06 +0800

    Imgur > Self host
    Co-authored-by: Mr Josh <dotmrjosh@users.noreply.github.com>

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -2,6 +2,7 @@
 	<title>Portfolio</title>
 </svelte:head>
 
-<p class="mx-auto w-11/12 min-h-100">
-	<img src="https://i.imgur.com/yxVbTJ2.jpeg" alt="aquarium date" />
+<p class="mx-auto w-11/12 min-h-100 items-center">
+	<!-- width = 3 sizes -->
+	<enhanced:img class="w-full" src="$lib/assets/aquarium-date.jpg?w=1280;640;400" alt="aquarium date" />
 </p>
