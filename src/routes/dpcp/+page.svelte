009b1cc3cb62b640af777b9fe3044b47213452c0
cosyreii <reishi2023@gmail.com>
2026-01-10 21:23:18 +0800

    setting up full project pages

--- src/routes/dpcp/+page.svelte
+++ src/routes/dpcp/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>DupCup</title>
+</svelte:head>
+
 <div class="mx-auto mb-8 flex min-h-100 w-11/12 flex-col items-center gap-2">
 	<div
 		class="flex flex-row items-center rounded-full border-2 border-dashed border-yellucent px-4 py-2 whitespace-nowrap"
---
+++ src/routes/ssl/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Super Streamer Lobby</title>
+</svelte:head>
+
+<div class="mx-auto mb-8 flex min-h-100 w-11/12 flex-col items-center gap-2">
+hi nothing here for now
+</div>
