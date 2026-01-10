c8349bb7e9d3126bb8b8f5bf4ff294aa1bda79ad
cosyreii <reishi2023@gmail.com>
2026-01-10 20:49:39 +0800

    cliackable experiences!

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,57 +1,67 @@
 <script>
 	import { HugeiconsIcon } from '@hugeicons/svelte';
-	import {
-		StarsIcon,
-		TimeQuarterPassIcon,
-		Time04Icon
-	} from '@hugeicons/core-free-icons';
+	import { StarsIcon, TimeQuarterPassIcon, Time04Icon } from '@hugeicons/core-free-icons';
 
 	const current = [
 		{
 			id: 'WBB',
 			name: "Wylie's Blitz Battle",
 			role: '2D Game Artist & Animator',
-			date: 'Jan 2026 - Present'
+			date: 'Jan 2026 - Present',
+			link: '',
+			target: ''
 		},
 		{
 			id: 'ToyboxTumble',
 			name: 'Toybox Tumble',
 			role: 'Creative & Design Director',
-			date: 'May 2025 - Present'
+			date: 'May 2025 - Present',
+			link: '',
+			target: ''
 		},
 		{
 			id: 'Freelance',
 			name: 'Freelance Multidisciplinary Artist',
 			role: 'Illustrator, Animator & Visual Designer',
-			date: 'May 2023 - Present'
+			date: 'May 2023 - Present',
+			link: '',
+			target: ''
 		}
 	];
 
 	const complete = [
-				{
+		{
 			id: 'DPCP',
 			name: 'DupCup [TETR.IO]',
 			role: 'Event Aesthetic & Promotional Artist',
-			date: 'Feb 2025 - Jan 2026'
+			date: 'Feb 2025 - Jan 2026',
+			link: '/dpcp',
+			target: ''
 		},
 		{
 			id: 'Intern1',
 			name: 'Lucky Mall MY',
 			role: 'Graphic Design Intern',
-			date: 'Sept 2025 - Dec 2025'
+			date: 'Sept 2025 - Dec 2025',
+			link: '',
+			target: ''
 		},
 		{
 			id: 'Diploma',
 			name: 'Asia Pacific University of Innovation and Technology',
 			role: 'Diploma in Media and Design (Animation)',
-			date: 'Aug 2023 - Aug 2025'
+			date: 'Aug 2023 - Aug 2025',
+			link: '',
+			target: ''
 		},
 		{
 			id: 'SSL',
 			name: 'Super Streamer Lobby [TETR.IO]',
 			role: 'Gamemode & Merchandise Illustrator',
-			date: 'Apr 2024 - Oct 2024'
-		},
+			date: 'Apr 2024 - Oct 2024',
+			link: '/ssl',
+			target: ''
+		}
 	];
 </script>
 
@@ -59,7 +69,7 @@
 	<title>Experience</title>
 </svelte:head>
 
-<div class="flex min-h-100 w-full items-start justify-center">
+<div class="flex min-h-100 w-full items-start justify-center mb-8">
 	<div class="flex w-11/12 flex-col">
 		<div class="mb-2 flex flex-row items-center justify-center text-translucent">
 			<HugeiconsIcon icon={TimeQuarterPassIcon} size="20" strokeWidth={1} />
@@ -69,19 +79,21 @@
 		{#each current as cur}
 			<div id={cur.id} class="mb-3 flex flex-col">
 				<div class="flex flex-row items-center">
-					<p class="flex flex-col text-left whitespace-nowrap">
-						<span class="text-2xl font-bold">{cur.name}</span>
-						<span class="text-yellow">{cur.role}</span>
-					</p>
+					<a href={cur.link} target={cur.target} class="w-full">
+						<p class="flex flex-col text-left whitespace-nowrap">
+							<span class="text-2xl font-bold">{cur.name}</span>
+							<span class="text-yellow">{cur.role}</span>
+						</p>
 
-					<hr class="mr-2 ml-8 w-full items-center border-dotted border-translucent" />
+						<hr class="items-center border-dotted border-translucent" />
 
-					<p
-						class="flex flex-row items-center justify-end gap-1 text-right whitespace-nowrap text-translucent italic"
-					>
-						<HugeiconsIcon icon={StarsIcon} size="15" strokeWidth={1} />
-						{cur.date}
-					</p>
+						<p
+							class="flex flex-row items-center justify-end gap-1 text-right whitespace-nowrap text-translucent italic"
+						>
+							<HugeiconsIcon icon={StarsIcon} size="15" strokeWidth={1} />
+							{cur.date}
+						</p>
+					</a>
 				</div>
 			</div>
 		{/each}
@@ -94,19 +106,21 @@
 		{#each complete as com}
 			<div id={com.id} class="mb-3 flex flex-col">
 				<div class="flex flex-row items-center">
-					<p class="flex flex-col text-left whitespace-nowrap">
-						<span class="text-2xl font-bold">{com.name}</span>
-						<span class="text-yellow">{com.role}</span>
-					</p>
+					<a href={com.link} target={com.target} class="w-full">
+						<p class="flex flex-col text-left whitespace-nowrap">
+							<span class="text-2xl font-bold">{com.name}</span>
+							<span class="text-yellow">{com.role}</span>
+						</p>
 
-					<hr class="mr-2 ml-8 w-full items-center border-dotted border-translucent" />
+						<hr class="items-center border-dotted border-translucent" />
 
-					<p
-						class="flex flex-row items-center justify-end gap-1 text-right whitespace-nowrap text-translucent italic"
-					>
-						<HugeiconsIcon icon={StarsIcon} size="15" strokeWidth={1} />
-						{com.date}
-					</p>
+						<p
+							class="flex flex-row items-center justify-end gap-1 text-right whitespace-nowrap text-translucent italic"
+						>
+							<HugeiconsIcon icon={StarsIcon} size="15" strokeWidth={1} />
+							{com.date}
+						</p>
+					</a>
 				</div>
 			</div>
 		{/each}
